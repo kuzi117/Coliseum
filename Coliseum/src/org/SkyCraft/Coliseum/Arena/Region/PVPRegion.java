7674372c29b228ddf77df57fd4981bf27fbb21c5
kuzi117 <puckmaster_84@hotmail.com>
2012-06-02 16:36:31 -0600

    Abstracted all team related things for extensibility.
    Signed-off-by: kuzi117 <puckmaster_84@hotmail.com>

--- Coliseum/src/org/SkyCraft/Coliseum/Arena/Region/PVPRegion.java
+++ Coliseum/src/org/SkyCraft/Coliseum/Arena/Region/PVPRegion.java
@@ -1,34 +1,12 @@
 package org.SkyCraft.Coliseum.Arena.Region;
 
-import java.util.HashMap;
-import org.bukkit.Location;
-
-public class PVPRegion extends Region {
-
-	private HashMap<String, Location> teamSpawns;
+public class PVPRegion extends ArenaRegion {
 	
 	public PVPRegion() {
 		super();
-		teamSpawns = new HashMap<String, Location>();
-	}
-	
-	public void addTeamSpawn(String name, Location loc) {
-		teamSpawns.put(name, loc.add(.5, 1, .5));
-		return;
-	}
-	
-	public void removeTeamSpawn(String name) {
-		teamSpawns.remove(name);
-	}
-	
-	public Location getTeamSpawn(String name) {
-		return teamSpawns.get(name);
 	}
 	
 	public boolean isCompleteRegion(int teamSize) {
-		if(teamSpawns.size() != teamSize) {
-			return false;
-		}
-		return super.isCompleteRegion();
+		return super.isCompleteRegion(teamSize);
 	}
 }
