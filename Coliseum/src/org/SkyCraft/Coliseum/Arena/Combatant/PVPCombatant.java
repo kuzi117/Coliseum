21d9f7e2ff6328324d60935047bca8fc3e9637cf
kuzi117 <puckmaster_84@hotmail.com>
2012-06-05 22:36:19 -0600

    Moved joinTeam to super.
    Signed-off-by: kuzi117 <puckmaster_84@hotmail.com>

--- Coliseum/src/org/SkyCraft/Coliseum/Arena/Combatant/PVPCombatant.java
+++ Coliseum/src/org/SkyCraft/Coliseum/Arena/Combatant/PVPCombatant.java
@@ -9,11 +9,6 @@ public class PVPCombatant extends Combatant {
 	public PVPCombatant(Player player) {
 		super(player);
 	}
-	
-	public void joinTeam(String team) {
-		this.team = team;
-		return;
-	}
 
 	public void toTeamSpawn(PVPRegion arenaRegion) {
 		player.teleport(arenaRegion.getTeamSpawn(team), TeleportCause.PLUGIN);
