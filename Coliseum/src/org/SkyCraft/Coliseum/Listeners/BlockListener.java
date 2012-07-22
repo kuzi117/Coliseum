164943793892d2aa380f80a68896aee453bda689
kuzi117 <puckmaster_84@hotmail.com>
2012-07-21 23:50:40 -0600

    Implemented what appears to be working BlockListener.
    Signed-off-by: kuzi117 <puckmaster_84@hotmail.com>

---
+++ Coliseum/src/org/SkyCraft/Coliseum/Listeners/BlockListener.java
@@ -0,0 +1,35 @@
+package org.SkyCraft.Coliseum.Listeners;
+
+import org.SkyCraft.Coliseum.ColiseumPlugin;
+import org.SkyCraft.Coliseum.Arena.Arena;
+import org.bukkit.ChatColor;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class BlockListener implements Listener {
+
+	ColiseumPlugin plugin;
+
+	public BlockListener(ColiseumPlugin plugin) {
+		this.plugin = plugin;
+	}
+	
+	@EventHandler
+	public void handleBlockBreak(BlockBreakEvent e) {
+		Player breaker = e.getPlayer();
+		Block block = e.getBlock();
+		
+		for(Arena a: plugin.getArenaSet()) {
+			if(a.hasThisPlayer(breaker) || 
+					(a.getRegion().isBlockContained(block.getLocation()) && a.isEnabled()/* && player has a permission*/) || 
+					(a.getWaitingRegion().isBlockContained(block.getLocation()) && a.isEnabled())/* && player has a permission*/) {
+				e.setCancelled(true);
+				breaker.sendMessage(ChatColor.GRAY + "[Coliseum] This block was not allowed to be broken.");
+			}
+		}
+		
+	}
+}
